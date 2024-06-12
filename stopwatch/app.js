e881183be38c5c64bd3e8d591200ea00575dc775
nayan97 <nayanislam085@gmail.com>
2024-06-12 08:54:31 +0600

    stop watch watach done

---
+++ stopwatch/app.js
@@ -0,0 +1,52 @@
+const display = document.getElementById('display');
+
+let timer = null;
+let startTime = 0;
+let esapsedTime = 0;
+let isRunning = false;
+
+
+function start() {
+    if (!isRunning){
+        startTime = Date.now() - esapsedTime;
+        timer =setInterval(update,10);
+     
+        isRunning = true; 
+    }
+}
+
+function pause() {
+    if (isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false; 
+    }
+
+}
+
+function reset() {
+    clearInterval(timer);
+    startTime = 0;
+    esapsedTime = 0;
+    isRunning = false;
+    display.textContent = '00:00:00:00';
+}
+
+function update() {
+
+    const curentTime = Date.now();
+    elapsedTime = curentTime - startTime;
+
+    let hours = Math.floor(elapsedTime/(1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime/(1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime/1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, '0');
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0');
+    milliseconds = String(milliseconds).padStart(2, '0');
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
