8c3a93f7dedbd928d8cd86c3024668e83cb42a51
nayan97 <nayanislam085@gmail.com>
2024-06-21 19:55:59 +0600

    sticky header

--- mycv/app.js
+++ mycv/app.js
@@ -1,5 +1,29 @@
+// toggle icon navbar
 
+// scroll Sections 
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if (top >= offset && top < offset + height) {
+
+            // active navber linkes
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        }
+    });
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+} 
 
