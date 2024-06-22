d1dd1b315ea0f7ddc40c919635472d7268776342
nayan97 <nayanislam085@gmail.com>
2024-06-22 20:11:40 +0600

    header menu

--- mycv/app.js
+++ mycv/app.js
@@ -1,5 +1,13 @@
 // toggle icon navbar
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 // scroll Sections 
 
 let sections = document.querySelectorAll('section');
@@ -25,5 +33,11 @@ window.onscroll = () => {
     });
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle button and navbar when click navbar links
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active')
+    
 } 
 
