fc95e9348717e5afd51805492ff659450bb221ed
nayan97 <nayanislam085@gmail.com>
2024-06-24 23:06:11 +0600

    analog clock

---
+++ watch/app.js
@@ -0,0 +1,24 @@
+
+
+const secondHand = document.getElementById('second-hand');
+const minuteHand = document.getElementById('minute-hand');
+const hourHand = document.getElementById('hour-hand');
+
+function ghoriGhonta(){
+    const date = new Date();
+    const seconds = date.getSeconds() / 60;
+    const minutes = (seconds + date.getMinutes()) / 60;
+    const hours = (minutes + date.getHours()) / 12;
+
+    rotateClockHand(secondHand, seconds);
+    rotateClockHand(minuteHand, minutes);
+    rotateClockHand(hourHand, hours);
+
+}
+function rotateClockHand(element, rotation){
+    element.style.setProperty('--rotate', rotation *360);
+
+}
+setInterval(ghoriGhonta, 1000);
+
+
