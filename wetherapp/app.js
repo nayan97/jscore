e1860347b0b4bb3a208e8d473f69ca4b54964741
nayan97 <nayanislam085@gmail.com>
2024-06-27 18:19:12 +0600

    basic weather app done

--- wetherapp/app.js
+++ wetherapp/app.js
@@ -4,6 +4,7 @@ const container = document.querySelector('.container');
 const search  = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
+const notFound = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
     const APIKey = 'c5bbfa5d9776025f97c4a388d1f723e2';
@@ -11,14 +12,27 @@ search.addEventListener('click', () => {
 
     if (city === '')
         return;
+//
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`).then(response => response.json()).then(json => {
+        if (json.cod == '404'){
+            container.style.height = '420px';
+            weatherBox.classList.remove('active');
+            weatherDetails.classList.remove('active');
+            notFound.classList.add('active');
+            return;
+        }
+            container.style.height = '570px';
+            weatherBox.classList.add('active');
+            weatherDetails.classList.add('active');
+            notFound.classList.remove('active');
+
 
         const image = document.querySelector('.weather-box img');
         const tempareture = document.querySelector('.weather-box .tempareture');
         const description = document.querySelector('.weather-box .description');
-        const humidity = document.querySelector('.weather-box .humidity span');
-        const wind = document.querySelector('.weather-box .wind span');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
 
         switch (json.weather[0].main) {
             case 'Clear':
@@ -48,6 +62,11 @@ search.addEventListener('click', () => {
                 image.src = 'img/bg-img.png';
                 break; 
         }
+
+        tempareture.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     })
 
 });
