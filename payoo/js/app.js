7209eb0b684fd4301678068835e4b2e6bad2b3f2
nayan97 <nayanislam085@gmail.com>
2025-02-27 18:43:43 +0600

    ac and pin validation

---
+++ payoo/js/app.js
@@ -0,0 +1,23 @@
+document.getElementById("login-btn")
+.addEventListener("click", function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("ac-number").value;
+ 
+    const pinNumber = document.getElementById("pin-number").value;
+    const pin = parseInt(pinNumber, 10);
+    
+    if (accountNumber.length === 11) {
+        if ( pin === 1234){
+
+            
+        }else {
+            console.log("Please enter a valid pin number");
+            
+        }
+        
+    }else {
+        console.log('invalid');
+        
+    }
+
+});
